2124edc1c71fe9710b4f23285834659ae5469f37
Khalid Abdulrahman Ali Elsheikh <khalid@webtronex.ca>
2025-03-10 06:54:58 -0400

    run.sh

--- odoo16/run.sh
+++ odoo16/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Odoo 16 Deployment Script with Postgres Database, custom folder for Docker files,
-# auto-generation of a minimal config file, and a command to fix permissions.
+# auto-generation of a minimal config file, and permission fix for host volumes.
 #
 # Usage:
 #   curl -s https://raw.githubusercontent.com/webTronex/odoo-docker-compose/main/odoo16/run.sh | sudo bash -s <instance_name> <ODOO_PORT> <LIVE_CHAT_PORT> <docker_folder>
@@ -40,4 +40,50 @@ export INSTANCE_NAME ODOO_PORT LIVE_CHAT_PORT ODOO_VERSION MASTER_PASSWORD
 cat <<EOF | envsubst > docker-compose.yml
 services:
   db-\${INSTANCE_NAME}:
-    image: postgres
+    image: postgres:14
+    environment:
+      - POSTGRES_USER=odoo
+      - POSTGRES_PASSWORD=odoo
+      - POSTGRES_DB=postgres
+    volumes:
+      - ./data_db_\${ODOO_PORT}:/var/lib/postgresql/data
+
+  odoo-\${INSTANCE_NAME}:
+    image: odoo:\${ODOO_VERSION}
+    depends_on:
+      - db-\${INSTANCE_NAME}
+    ports:
+      - "\${ODOO_PORT}:8069"
+      - "\${LIVE_CHAT_PORT}:8072"
+    environment:
+      - DB_HOST=db-\${INSTANCE_NAME}
+      - DB_PORT=5432
+      - DB_USER=odoo
+      - DB_PASSWORD=odoo
+      - MASTER_PASSWORD=\${MASTER_PASSWORD}
+    volumes:
+      - ./addons_\${ODOO_PORT}:/mnt/extra-addons
+      - ./config:/etc/odoo
+      - ./data:/var/lib/odoo
+EOF
+
+mkdir -p addons_"$ODOO_PORT" config data data_db_"$ODOO_PORT"
+
+sudo chown -R 999:999 addons_"$ODOO_PORT" config data data_db_"$ODOO_PORT"
+
+if [ ! -f config/odoo.conf ]; then
+  cat > config/odoo.conf <<EOC
+[options]
+db_host = db-${INSTANCE_NAME}
+db_port = 5432
+db_user = odoo
+db_password = odoo
+addons_path = /mnt/extra-addons
+EOC
+fi
+
+docker compose up -d
+
+echo "Odoo ${ODOO_VERSION} instance '${INSTANCE_NAME}' deployed in folder '${DOCKER_FOLDER}'!"
+echo "Access URL: http://localhost:${ODOO_PORT}"
+echo "Master Password: ${MASTER_PASSWORD}"
