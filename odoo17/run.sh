0f43131cee32cfda40f04b64d886929f41f91d9f
Khalid Abdulrahman Ali Elsheikh <khalid@webtronex.ca>
2025-03-10 05:07:43 -0400

    run.sh

--- odoo17/run.sh
+++ odoo17/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Odoo 17 Deployment Script with Postgres Database and custom folder for Docker files
-# Usage: curl -s https://raw.githubusercontent.com/<your-repo>/odoo17/run.sh | sudo bash -s <instance_name> <ODOO_PORT> <LIVE_CHAT_PORT> <docker_folder>
+# Usage: curl -s https://raw.githubusercontent.com/webTronex/odoo-docker-compose/main/odoo17/run.sh | sudo bash -s <instance_name> <ODOO_PORT> <LIVE_CHAT_PORT> <docker_folder>
 # Example: sudo bash run.sh odoo-main 1017 2017 my_odoo17_instance
 
 set -e
@@ -11,61 +11,61 @@ if [ "$#" -ne 4 ]; then
     exit 1
 fi
 
-INSTANCE_NAME=$1
-ODOO_PORT=$2
-LIVE_CHAT_PORT=$3
-DOCKER_FOLDER=$4
+INSTANCE_NAME="$1"
+ODOO_PORT="$2"
+LIVE_CHAT_PORT="$3"
+DOCKER_FOLDER="$4"
 ODOO_VERSION="17.0"
 MASTER_PASSWORD="admin"
 
 check_port() {
-    if lsof -i :$1 >/dev/null; then
-        echo "Port $1 is already in use! Choose different ports."
+    if lsof -i :"${1}" >/dev/null; then
+        echo "Port ${1} is already in use! Choose different ports."
         exit 1
     fi
 }
 
-check_port $ODOO_PORT
-check_port $LIVE_CHAT_PORT
+check_port "${ODOO_PORT}"
+check_port "${LIVE_CHAT_PORT}"
 
-mkdir -p "$DOCKER_FOLDER"
-cd "$DOCKER_FOLDER"
+mkdir -p "${DOCKER_FOLDER}"
+cd "${DOCKER_FOLDER}"
 
 cat > docker-compose.yml <<EOF
 version: '3'
 services:
-  db-\$INSTANCE_NAME:
+  db-${INSTANCE_NAME}:
     image: postgres:14
     environment:
       - POSTGRES_USER=odoo
       - POSTGRES_PASSWORD=odoo
       - POSTGRES_DB=postgres
     volumes:
-      - ./data_db_\$ODOO_PORT:/var/lib/postgresql/data
+      - ./data_db_${ODOO_PORT}:/var/lib/postgresql/data
 
-  odoo-\$INSTANCE_NAME:
-    image: odoo:\$ODOO_VERSION
+  odoo-${INSTANCE_NAME}:
+    image: odoo:${ODOO_VERSION}
     depends_on:
-      - db-\$INSTANCE_NAME
+      - db-${INSTANCE_NAME}
     ports:
-      - "\$ODOO_PORT:8069"
-      - "\$LIVE_CHAT_PORT:8072"
+      - "${ODOO_PORT}:8069"
+      - "${LIVE_CHAT_PORT}:8072"
     environment:
-      - DB_HOST=db-\$INSTANCE_NAME
+      - DB_HOST=db-${INSTANCE_NAME}
       - DB_PORT=5432
       - DB_USER=odoo
       - DB_PASSWORD=odoo
-      - MASTER_PASSWORD=\$MASTER_PASSWORD
+      - MASTER_PASSWORD=${MASTER_PASSWORD}
     volumes:
-      - ./addons_\$ODOO_PORT:/mnt/extra-addons
+      - ./addons_${ODOO_PORT}:/mnt/extra-addons
       - ./config:/etc/odoo
       - ./data:/var/lib/odoo
 EOF
 
-mkdir -p addons_$ODOO_PORT config data data_db_$ODOO_PORT
+mkdir -p addons_${ODOO_PORT} config data data_db_${ODOO_PORT}
 
 docker compose up -d
 
-echo "Odoo \$ODOO_VERSION instance '\$INSTANCE_NAME' deployed in folder '$DOCKER_FOLDER'!"
-echo "Access URL: http://localhost:\$ODOO_PORT"
-echo "Master Password: \$MASTER_PASSWORD"
+echo "Odoo ${ODOO_VERSION} instance '${INSTANCE_NAME}' deployed in folder '${DOCKER_FOLDER}'!"
+echo "Access URL: http://localhost:${ODOO_PORT}"
+echo "Master Password: ${MASTER_PASSWORD}"
